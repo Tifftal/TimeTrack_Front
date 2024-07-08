e418425de058517ea38debc0e40f105faac89599
VarTy <talankina.v@mail.ru>
2024-07-08 22:15:32 +0300

    add login page

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,17 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import './Auth.scss';
-import { Button } from "@mantine/core";
+import { Button, Image } from "@mantine/core";
+import IconLogo from '../../assets/IconLogo.svg';
+import Logo from '../../assets/Logo.svg'
 
 const Auth = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="auth">
             <div className="auth-navbar">
                 <div className="auth-navbar-logo">
-                    LOGO
+                    <Image
+                        h={'80%'}
+                        src={IconLogo}
+                    />
+                    <Image
+                        h={'80%'}
+                        src={Logo}
+                    />
                 </div>
                 <div className="auth-navbar-btn">
-                    <Button variant="transparent">Зарегистрироваться</Button>
-                    <Button>Войти</Button>
+                    <Button variant="transparent" onClick={() => navigate('register')}>Зарегистрироваться</Button>
+                    <Button onClick={() => navigate('login')}>Войти</Button>
                 </div>
             </div>
             <div className="auth-body">
--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,9 +1,44 @@
 import './Login.scss';
+import { Button, Image, TextInput, Text, PasswordInput, Checkbox } from "@mantine/core";
+import IconLogo from '../../../assets/IconLogo.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="login">
-            LOGIN
+            <div className='login-container'>
+                <Image
+                    h={48}
+                    w={48}
+                    src={IconLogo}
+                />
+                <Text size="28px" fw={600} mt={25}>Войдите в ваш аккаунт</Text>
+                <Text size="14px" fw={400} c={'gray'}>Welcome back! Please enter your details.</Text>
+                <TextInput
+                    w={'100%'}
+                    label="Логин"
+                    placeholder="Введите ваш логин"
+                />
+                <PasswordInput
+                    w={'100%'}
+                    label="Пароль"
+                    placeholder="Введите пароль"
+                    defaultVisible
+                />
+                <div className='login-container-btn'>
+                    <Checkbox
+                        label="Сохранить пароль"
+                    />
+                    <Button variant='transparent' p={0}>Забыли пароль?</Button>
+                </div>
+                <Button fullWidth>Войти</Button>
+                <div className='login-container-btn'>
+                <Text size="14px" fw={400}>Еще не зарегистрированы?</Text>
+                    <Button variant='transparent' p={0} onClick={()=>navigate('/auth/register')}>Зарегистрироваться</Button>
+                </div>
+            </div>
         </div>
     )
 }
