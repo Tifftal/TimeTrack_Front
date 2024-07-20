df38f706b7ea4725d6915a4676510c3f2a8187cc
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-07-21 00:02:27 +0300

    Updated typistation

--- src/shared/Container/types.ts
+++ src/shared/Container/types.ts
@@ -3,6 +3,7 @@ import { ReactNode } from "react"
 export type Props = {
   backgroundColor?: string,
   flexDirection?: 'row' | 'column',
+  color?: string;
   padding?: string,
   borderRadius?: string,
   height?: string,
